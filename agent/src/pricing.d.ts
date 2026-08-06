444d6e92a2ad4ab061d8b758d3f60b9570586458
Leticia Azevedo <leticiac@email.com>
2026-08-06 14:59:19 -0300

    chore: remove stray compiled TS output from src/, add outDir to tsconfig

--- agent/src/pricing.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const PRICING: {
-    readonly basic: 30000000;
-    readonly deep: 70000000;
-};
-//# sourceMappingURL=pricing.d.ts.map
--- agent/src/pricing.js
+++
@@ -1,5 +0,0 @@
-export const PRICING = {
-    basic: 30_000_000, // 3 XLM in stroops
-    deep: 70_000_000, // 7 XLM in stroops
-};
-//# sourceMappingURL=pricing.js.map
